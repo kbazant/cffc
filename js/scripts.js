975331e0c6782ad4f819193498f6e46e0476be1a
kbazant <krizz100@yahoo.com>
2016-11-11 00:59:47 +0000

    added javascript for converting celsius to fahrenheit

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-function convertToF(celcius) {
-	var fahrenheit;
-	
-	fahrenheit = celsius * 9/5 + 32;
-	
-	return fahrehneit;
-	
+function convert(degree) {
+	var temperature;
+	if (degree == "C") {
+		temperature = document.getElementById("c").value * 9 / 5 + 32;
+		document.getElementById("f").value = Math.round(temperature);
+		
+	} else {
+		temperature = (document.getElementById("f").value -32) * 5/9;
+		document.getElementById("c").value = Math.round(temperature);
+	}
 }
-
-convertToF(0);
+	
