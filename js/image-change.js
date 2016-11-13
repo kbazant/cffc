22dfa3a9da15e8f4e081e5aa80bc6cefae312f18
kbazant <krizz100@yahoo.com>
2016-11-13 23:32:14 +0000

    added testing script for different images

--- js/image-change.js
+++ js/image-change.js
@@ -1,44 +1,105 @@
-//image change testing
+////image change testing
+//
+//window.onload = function() { // because we are grabbing DOM elements we need to ensure the elements are first loaded.
+//    var elm = document.getElementById("c"),
+//        img = document.getElementById("myimg"); // here we grab the input box and the image
+//    elm.onkeyup = function(event) { // capture the input keyup
+//        console.log(this.value.length);
+//        if (this.value.length === 1) { // make sure we are only checking the first value
+//            switch (event.keyCode) { // here we are running the switch statement against the keyup event keycode value
+//            case (<10): 
+//                img.src = "images/desert-africa.jpg";
+//                break;
+//            case (>=11):
+//                 img.src = "images/arctic-cold.jpg";
+//                break;
+//            case (>=20):
+//                 img.src = "images/cherry-blossom.jpg";
+//                break;
+//            }
+//        }
+//    }
+//
+//}
 
-window.onload = function() { // because we are grabbing DOM elements we need to ensure the elements are first loaded.
-    var elm = document.getElementById("c"),
-        img = document.getElementById("myimg"); // here we grab the input box and the image
-    elm.onkeyup = function(event) { // capture the input keyup
-        console.log(this.value.length);
-        if (this.value.length === 1) { // make sure we are only checking the first value
-            switch (event.keyCode) { // here we are running the switch statement against the keyup event keycode value
-            case (48): 
-                img.src = "http://dummyimage.com/100x100/000/fff&text=0";
-                break;
-            case (49):
-                 img.src = "images/arctic-cold.jpg";
-                break;
-            case (50):
-                 img.src = "http://dummyimage.com/100x100/000/fff&text=2";
-                break;
-            case (51):
-                 img.src = "http://dummyimage.com/100x100/000/fff&text=3";
-                break;
-            case (52):
-                 img.src = "http://dummyimage.com/100x100/000/fff&text=4";
-                break;
-            case (53):
-                 img.src = "http://dummyimage.com/100x100/000/fff&text=5";
-                break;
-            case (54):
-                 img.src = "http://dummyimage.com/100x100/000/fff&text=6";
-                break;
-            case (55):
-                 img.src = "http://dummyimage.com/100x100/000/fff&text=7";
-                break;
-            case (56):
-                 img.src = "http://dummyimage.com/100x100/000/fff&text=8";
-                break;
-            case (57):
-                 img.src = "http://dummyimage.com/100x100/000/fff&text=9";
-                break;
-            }
-        }
-    }
+$(document).ready(function() {
+  
+  $("#c").on("input",function(){
+    var value = $(this).val();
+		
+//    1. Temperature of -20C or less
+    if (value <== "-20") 
+      $(".wrapper").css({backgroundImage:"url(images/arctic-cold.jpg)"});
+		
+//    2. Temperature between -19C and -10C
+    else if (value == "-19")
+      $(".wrapper").css({backgroundImage:"url(images/winter-mountain-landscape.jpg)"});
+		
+//		3. Temperature between -9C and 0C
+		else if (value == "3")
+      $(".wrapper").css({backgroundImage:"url(images/snowboarding.jpg)"});
+		
+//		4. Temperature between 1C and 10C
+		else if (value == "4")
+      $(".wrapper").css({backgroundImage:"url(images/fall-autumn-season.jpg)"});
+		
+//		5. Temperature between 11C and 19C
+		else if (value == "5")
+      $(".wrapper").css({backgroundImage:"url(images/cherry-blossom.jpg)"});
+		
+//		6. Temperature between 20C and 29C
+		else if (value == "6")
+      $(".wrapper").css({backgroundImage:"url(images/tropical-beach-and-palm-tree.jpg)"});
+		
+//		7. Temperature of +30C or more
+		else if (value == "7")
+      $(".wrapper").css({backgroundImage:"url(images/desert-africa.jpg)"});
+            
+        })
+	
+	
+//	$("#f").on("input",function(){
+//    var value = $(this).val();
+//		
+////    1. Temperature of -20C or less
+//    if (value <= "1") 
+//      $(".wrapper").css({backgroundImage:"url(images/arctic-cold.jpg)"});
+//		
+//
+//            
+//        })
+	
+	
+	
+                
+    })
 
-}
+
+//  1. <= -20**
+//	2. -19 - -10**
+//	3. -9 - 0**
+//	4. 1 - 10**
+//	5. 11 - 19**
+//	6. 20 - 30
+//	7. 30 =>
+
+
+
+
+
+//	The image change depending on temperature:
+//	1. <= -20
+//	2. -19 - -10
+//	3. -9 - 0
+//	4. 1 - 10
+//	5. 11 - 19
+//	6. 20 - 30
+//	7. 30 =>
+
+
+
+
+
+//<div class="jp-front">
+//  <input type="text" name="" class="credit-input">
+//</div>
