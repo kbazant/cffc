ed8ffd5aaf69f4c6db66b6e000838bfd989639bc
kbazant <krizz100@yahoo.com>
2016-11-15 19:59:56 +0000

    set different pictures depending in temperature input
    The background picture change when different temperature is set by the
    user. It changes every 10 degrees.

--- js/image-change.js
+++ js/image-change.js
@@ -28,11 +28,11 @@ $(document).ready(function() {
     var value = $(this).val();
 		
 //    1. Temperature of -20C or less
-    if (value <= -20) 
+    if (value < -19) 
       $(".wrapper").css({backgroundImage:"url(images/arctic-cold.jpg)"});
 		
 //    2. Temperature between -19C and -10C
-    else if (value > -20 && value < -10)
+    else if (value > -20 && value < -9)
       $(".wrapper").css({backgroundImage:"url(images/winter-mountain-landscape.jpg)"});
 		
 //		3. Temperature between -9C and 0C
@@ -44,15 +44,15 @@ $(document).ready(function() {
       $(".wrapper").css({backgroundImage:"url(images/fall-autumn-season.jpg)"});
 		
 //		5. Temperature between 11C and 19C
-		else if (value > 10 && value < 20)
+		else if (value > 10 && value < 21)
       $(".wrapper").css({backgroundImage:"url(images/cherry-blossom.jpg)"});
 		
 //		6. Temperature between 20C and 29C
-		else if (value > 19 && value < 30)
+		else if (value > 19 && value < 31)
       $(".wrapper").css({backgroundImage:"url(images/tropical-beach-and-palm-tree.jpg)"});
 		
 //		7. Temperature of +30C or more
-		else if (value > 29)
+		else if (value > 30)
       $(".wrapper").css({backgroundImage:"url(images/desert-africa.jpg)"});
             
         })
